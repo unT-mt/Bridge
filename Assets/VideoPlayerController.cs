9a1ae9264fc9c6c4fd73759241def8c1ed80636c
DESKTOP-H1UGF5O\m-takashima <m-takashima@un-t.com>
2024-09-03 18:11:19 +0900

    デスクトップのパスを取得してフォルダにアクセスする その他コメント追加

--- Assets/VideoPlayerController.cs
+++ Assets/VideoPlayerController.cs
@@ -1,35 +1,53 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
 
 public class VideoPlayerController : MonoBehaviour
 {
-    public string videoPath = "file://C:/Users/m-takashima/Desktop/Sample/000.mp4";
+    //ファイル名の指定（現状は仮でpublicで指定）
+    public string videoPath = "Sample/000.mp4";
+    public string imagePath001 = "Sample/001.png";
+    public string imagePath002 = "Sample/002.png";
+    public string imagePath003 = "Sample/003.png";
+
+    //動画と画像を表示するレンダーテクスチャを指定
     public RenderTexture renderTexture;
 
-    public string imagePath001 = "file://C:/Users/m-takashima/Desktop/Sample/001.png";
-    public string imagePath002 = "file://C:/Users/m-takashima/Desktop/Sample/002.png";
-    public string imagePath003 = "file://C:/Users/m-takashima/Desktop/Sample/003.png";
-
+    //自オブジェクトにアタッチするコンポーネント
     private VideoPlayer videoPlayer;
     private RawImage rawImage;
     private Texture2D imageTexture;
 
     void Start()
     {
-        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        //システムからパスを取得（現状は仮でDesktopを指定）
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        Debug.Log("Desktop Path: " + desktopPath);
+
+        // パスを結合してフルパスを生成
+        videoPath = "file://" + System.IO.Path.Combine(desktopPath, videoPath);
+        imagePath001 = "file://" + System.IO.Path.Combine(desktopPath, imagePath001);
+        imagePath002 = "file://" + System.IO.Path.Combine(desktopPath, imagePath002);
+        imagePath003 = "file://" + System.IO.Path.Combine(desktopPath, imagePath003);
+
+        //コンポーネントの取得
+        videoPlayer = gameObject.GetComponent<VideoPlayer>();
         rawImage = gameObject.GetComponent<RawImage>();
         imageTexture = new Texture2D(2, 2);
 
         // 動画の再生終了時のコールバックを設定
         videoPlayer.loopPointReached += OnVideoEnd;
 
+        //初回起動時はビデオを再生
         PlayVideo();
     }
 
     void Update()
     {
+        //キーに対応した処理（現状は仮。今後キーマッピング追加予定（戻る/進む/Topへ））
         if (Input.GetKeyDown(KeyCode.Z))
         {
             DisplayImage(imagePath001);
@@ -48,6 +66,11 @@ public class VideoPlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// レンダーテクスチャを利用してビデオを再生する
+    /// 未実装：Vキー押下時、動画再生中なら動画の再度再生をしない
+    /// 未実装：テキストファイルを生成し入力デバイスのボタンを光らせる
+    /// </summary>
     private void PlayVideo()
     {
         rawImage.texture = renderTexture;
@@ -56,12 +79,26 @@ public class VideoPlayerController : MonoBehaviour
         videoPlayer.Play();
     }
 
+    /// <summary>
+    /// 画像を表示する
+    /// 未実装：フェードインアウト
+    /// 未実装：表示切替時の効果音
+    /// 未実装：所定の時間経過時にTopに戻る（所定の時間はconfigテキストで指定するようにする）
+    /// </summary>
+    /// <param name="imagePath"></param>
     private void DisplayImage(string imagePath)
     {
         StartCoroutine(LoadImage(imagePath));
         videoPlayer.Stop();
     }
 
+    /// <summary>
+    /// 画像の取得
+    /// 現状仮のコーディングのため一旦wwwクラスで動くことを確認
+    /// Web通信が不要の場合File.ReadAllBytesで動かすこと検証する
+    /// </summary>
+    /// <param name="imagePath"></param>
+    /// <returns></returns>
     private IEnumerator LoadImage(string imagePath)
     {
         using (WWW www = new WWW(imagePath))
@@ -72,8 +109,13 @@ public class VideoPlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// ビデオの終了時の処理（再度ビデオを再生）
+    /// </summary>
+    /// <param name="vp"></param>
     private void OnVideoEnd(VideoPlayer vp)
     {
-        PlayVideo(); // 動画再生終了時に再度再生
+        // 動画再生終了時に再度再生
+        PlayVideo(); 
     }
 }
